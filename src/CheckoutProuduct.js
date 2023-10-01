0307a79a01bd879e02b222c7441cd1748236fa34
sonu <sonugautam9699@gmail.com>
2023-10-01 20:10:59 +0530

    checking

--- src/CheckoutProuduct.js
+++ src/CheckoutProuduct.js
@@ -11,6 +11,7 @@ function CheckoutProuduct( props ) {
         });
     };
     return (
+        //proudct to show
         <div className='checkoutproduct'>
             <img className = 'checkoutproductimg' src={props.image}
                 alt='image not found'
